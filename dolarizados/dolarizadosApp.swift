8a3c567ce794484fe6aa1849ad1f9df73808f495
Leandro Bordon <leandrobordondev@gmail.com>
2024-08-25 00:57:32 -0300

    Initial Commit

---
+++ dolarizados/dolarizadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  dolarizadosApp.swift
+//  dolarizados
+//
+//  Created by Leandro Bordon on 25/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct dolarizadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
